228b82e1c70ba09dd8e2408cdb9fb9b0ebc22259
Tsutomu Kawamura <kawamura@cognitom.com>
2009-09-13 10:35:05 +0900

    インストーラのマニフェスト

---
+++ manifest.php
@@ -0,0 +1,29 @@
+<?php 
+$manifest = array( 
+	'acceptable_sugar_versions' => array (
+		'regex_matches' => array (
+			"5\.2\.0.*",
+		),
+	),
+	'acceptable_sugar_flavors' => array (
+		'CE',
+	),
+	'name' => 'JapaneseLanguagePack',
+	'description' => 'SugarCE Japanese Language Pack (lang-file-only)',
+	'type' => 'langpack',
+	'is_uninstallable' => true,
+	'version' => '5.2.0i',
+	'published_date' => '2009-09-13 00:00:00',
+	'author' => 'SugarForum.jp',
+);
+
+$installdefs = array(
+	'id'=> 'ja',
+	'image_dir'=>'<basepath>/images',
+	'copy' => array(
+		array('from'=> '<basepath>/include','to'=> 'include'),
+		array('from'=> '<basepath>/modules','to'=> 'modules'),
+		array('from'=> '<basepath>/install','to'=> 'install'),
+	)
+);
+?>
